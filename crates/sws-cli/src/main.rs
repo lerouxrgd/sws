94fef5ce1676d31213105e7b43bec052e098439e
Romain Leroux <romain.leroux@ubisoft.com>
2022-05-18 17:15:17 +0200

    Use clap instead of structopt

--- crates/sws-cli/src/main.rs
+++ crates/sws-cli/src/main.rs
@@ -1,62 +1,55 @@
 use std::io;
 use std::path::PathBuf;
 
-use structopt::{clap::AppSettings, clap::Shell, StructOpt};
+use clap::{CommandFactory, Parser};
+use clap_complete::{generate, Shell};
 use sws_crawler::crawl_site;
 use sws_lua::{LuaScraper, LuaScraperConfig};
 use tokio::runtime;
 
-// TODO: switch to full clap
+/// Sitemap Web Scraper
+#[derive(Debug, Parser)]
+#[clap(version)]
+pub struct Args {
+    #[clap(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum SubCommand {
+    #[clap(name = "scrap")]
+    Scrap(ScrapArgs),
+    #[clap(hide = true)]
+    Completion,
+}
 
 /// Scrap website content
-#[derive(Debug, StructOpt)]
-pub struct ScrapOpts {
+#[derive(Debug, clap::Args)]
+pub struct ScrapArgs {
     /// Path to the Lua script that defines scraping logic
-    #[structopt(parse(from_os_str))]
-    pub script_file: PathBuf,
-    /// Path where the scraping csv file will be written
-    #[structopt(parse(from_os_str))]
+    #[clap(parse(from_os_str), long, short)]
+    pub script: PathBuf,
+    /// Path to the output file that will contain scrapped data
+    #[clap(parse(from_os_str), long, short)]
     pub output_file: PathBuf,
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt()]
-pub enum Command {
-    #[structopt(name = "scrap")]
-    Scrap(ScrapOpts),
-    #[structopt(setting(AppSettings::Hidden))]
-    Completion,
-    #[structopt(setting(AppSettings::Hidden))]
-    Help,
-}
-
-/// Sitemap Web Scraper
-#[derive(Debug, StructOpt)]
-pub struct Opts {
-    #[structopt(subcommand)]
-    pub command: Command,
-}
-
-pub fn run_scrap(opts: ScrapOpts) -> anyhow::Result<()> {
-    let rt = runtime::Builder::new_multi_thread().enable_all().build()?;
-
-    let conf = LuaScraperConfig {
-        script: opts.script_file,
-        csv_file: opts.output_file,
+pub fn scrap(args: ScrapArgs) -> anyhow::Result<()> {
+    let scraper_conf = LuaScraperConfig {
+        script: args.script,
+        csv_file: args.output_file,
     };
-
-    rt.block_on(crawl_site::<LuaScraper>(&conf))?;
-
-    Ok(())
+    let rt = runtime::Builder::new_multi_thread().enable_all().build()?;
+    rt.block_on(crawl_site::<LuaScraper>(&scraper_conf))
 }
 
 fn main() -> anyhow::Result<()> {
-    match Opts::from_args().command {
-        Command::Scrap(opts) => run_scrap(opts),
-        Command::Completion => {
-            Opts::clap().gen_completions_to("sws", Shell::Bash, &mut io::stdout());
+    let args = Args::parse();
+    match args.cmd {
+        SubCommand::Scrap(args) => scrap(args),
+        SubCommand::Completion => {
+            generate(Shell::Bash, &mut Args::command(), "sws", &mut io::stdout());
             Ok(())
         }
-        Command::Help => Ok(()),
     }
 }
