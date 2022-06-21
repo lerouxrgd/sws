b560298c9bc25ac15e2695282c132ac49f14da30
Romain Leroux <romain.leroux@ubisoft.com>
2022-06-21 17:46:45 +0200

    Re-export anyhow in crawler and scraper

--- crates/sws-crawler/src/lib.rs
+++ crates/sws-crawler/src/lib.rs
@@ -5,3 +5,5 @@ mod scrapable;
 pub use config::{CrawlerConfig, OnError};
 pub use crawler::crawl_site;
 pub use scrapable::{CountedTx, PageLocation, Scrapable, Seed, Sitemap};
+
+pub use anyhow;
--- crates/sws-lua/src/lib.rs
+++ crates/sws-lua/src/lib.rs
@@ -4,3 +4,5 @@ mod scraper;
 pub mod writer;
 
 pub use scraper::{scrap_glob, scrap_page, LuaScraper, LuaScraperConfig};
+
+pub use anyhow;
